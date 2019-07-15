412ab108b1a39034c47dafdad5668722eb078207
Mohamed Samir <mosamircs@gmail.com>
2019-07-15 05:00:47 +0200

    our work

--- test.py
+++ test.py
@@ -6,3 +6,6 @@ print("sara")
 print("hesham")
 print("bedewy")
 print("the best team to be")
+
+#I need first the data set to be uploaded on Gdrive cloud
+#then i can work on project with the team by using the neural netwrk technique RNN or LSTM for videos
