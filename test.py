685b2190aab0a5d8e4d17f2207c6c193139f4fbf
Mohamed Samir <mosamircs@gmail.com>
2019-07-15 05:11:33 +0200

    first commit from pycharm

--- test.py
+++ test.py
@@ -9,3 +9,4 @@ print("the best team to be")
 
 #I need first the data set to be uploaded on Gdrive cloud
 #then i can work on project with the team by using the neural netwrk technique RNN or LSTM for videos
+#commit this for now
